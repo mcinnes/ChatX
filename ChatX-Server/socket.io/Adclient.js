24e26a57dd33b8ecae1661b90aeb4c34633247b8
Matt McInnes <matt@matts-macbook-pro-2.local>
2017-08-22 13:30:36 +1000

    Advetiser color, download images on chat start

--- ChatX-Server/socket.io/Adclient.js
+++ ChatX-Server/socket.io/Adclient.js
@@ -6,7 +6,7 @@ var socket = io.connect('http://127.0.0.1:3000', {reconnect: false});
 socket.on('connect', function (socket) {
     console.log('Connected!');
 });
-socket.emit('advertisment', {"roomNumber":"xZhNseTPAwvsROs","msg":"$4 coffee at Fusion for the next 40 minutes", "source":"FUSION Deakin"});
+socket.emit('advertisment', {"roomNumber":"ClqNenpVUUOrRUa","msg":"$4 coffee at Fusion for the next 40 minutes", "source":"FUSION Deakin"});
 
  socket.on("disconnect", function(){
         console.log("client disconnected from server");
