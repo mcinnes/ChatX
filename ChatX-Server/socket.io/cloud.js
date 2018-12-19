48ffb598586fe2a8b1f9fe77352d0e79357b7349
Matt McInnes <mcinnes@me.com>
2018-12-19 12:21:18 +1100

    Updates to cloud.js

--- ChatX-Server/socket.io/cloud.js
+++ ChatX-Server/socket.io/cloud.js
@@ -1,17 +1,24 @@
+//Functions in Parse/htdocs/cloud.js
+
+//Increase user count accessed at URL/function/increaseUserCount
 Parse.Cloud.define("increaseUserCount", function(request, response) {
+  //Select the table to query
   var query = new Parse.Query("CurrentChats");
+  //Select row with the correct ID for the chat
   query.equalTo("ChatRoomID", request.params.ChatRoomID);
   query.find({
     success: function(results) {
+      //Increment Count by 1
      results.increment("CurrentCount");
      results.save();
     },
     error: function() {
-      response.error("adding failed");
+      response.error("increment failed");
     }
   });
 });
 
+//Increase user count accessed at URL/function/decreaseUserCount
 Parse.Cloud.define("decreaseUserCount", function(request, response) {
   var query = new Parse.Query("CurrentChats");
   query.equalTo("ChatRoomID", request.params.ChatRoomID);
